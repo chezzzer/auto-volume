e3919047bc2f14e22080a76938496460be8548a9
chezzzer <46093784+chezzzer@users.noreply.github.com>
2023-03-02 18:45:36 +1300

    up

---
+++ src/index.ts
@@ -0,0 +1,106 @@
+import express, { Request, Response } from "express";
+import fs from "fs";
+import Spotify from "spotify-web-api-node";
+import open from "open";
+import "dotenv/config";
+import { Track } from "spotify-types";
+
+if (!fs.existsSync("VOLUME")) {
+    fs.writeFileSync("VOLUME", "0.3", "utf-8");
+}
+var average_volume = Number(fs.readFileSync("VOLUME"));
+
+let current: Track;
+
+const spotify = new Spotify({
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    redirectUri: process.env.SPOTIFY_REDIRECT,
+});
+
+async function refreshToken() {
+    try {
+        const token = await spotify.refreshAccessToken();
+        spotify.setAccessToken(token.body.access_token);
+    } catch (_) {
+        refreshToken();
+    }
+}
+
+async function main() {
+    try {
+        const state = await spotify.getMyCurrentPlaybackState();
+
+        if (current?.id !== state.body.item?.id) {
+            current = state.body.item as any;
+
+            await autoVolume();
+        }
+    } catch (_) {}
+}
+
+async function autoVolume() {
+    const analysis = await spotify.getAudioFeaturesForTrack(current.id);
+    const { loudness } = analysis.body;
+
+    const percent = loudness / -60;
+
+    const calculatedPercent = Math.floor((average_volume + percent) * 100);
+
+    console.log(`Loudness: ${loudness}db\nNew Volume: ${calculatedPercent}%`);
+
+    await spotify.setVolume(calculatedPercent);
+}
+
+const app = express();
+
+app.get("/api/setvolume/:volume", (req: Request, res: Response) => {
+    if (isNaN(Number(req.params.volume))) {
+        return res.end("Not a number");
+    }
+
+    fs.writeFileSync("VOLUME", req.params.volume);
+    average_volume = Number(req.params.volume);
+
+    autoVolume();
+
+    res.end(`Average volume updated to ${req.params.volume}%`);
+});
+
+if (fs.existsSync("TOKEN") || process.env.SPOTIFY_REFRESH) {
+    const refresh =
+        fs.readFileSync("TOKEN", "utf-8") || process.env.SPOTIFY_REFRESH;
+    spotify.setRefreshToken(refresh!);
+    await refreshToken();
+
+    setInterval(refreshToken, 59 * 60 * 1000);
+    setInterval(main, 1 * 1000);
+} else {
+    open(
+        spotify.createAuthorizeURL(
+            ["user-modify-playback-state", "user-read-playback-state"],
+            "g3yb8547g485"
+        )
+    );
+
+    app.get("/api/callback", async (req: Request, res: Response) => {
+        const auth = await spotify.authorizationCodeGrant(
+            req.query.code as string
+        );
+
+        const token = auth.body;
+
+        fs.writeFileSync("TOKEN", token.refresh_token);
+
+        spotify.setRefreshToken(token.refresh_token);
+        spotify.setAccessToken(token.access_token);
+
+        setInterval(main, 1 * 1000);
+
+        res.end("Success, now tracking.");
+    });
+}
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Listening");
+});
